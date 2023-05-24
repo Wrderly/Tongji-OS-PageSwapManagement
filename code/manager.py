eb2a5003dbf1f583e049cd20ebd7032e12d1c0a5
Wrderly <97280890+Wrderly@users.noreply.github.com>
2023-05-24 20:18:40 +0800

    代码优化
    对代码中的一些重复部分进行了合并，对一些冗余的代码进行了精简。修改了一些无意义的变量名，并补充了必要的注释，以提高代码可读性。删除了部分已经失效的函数

--- code/manager.py
+++ code/manager.py
@@ -1,16 +1,6 @@
 import queue
 
 
-def managerLog(code_num, current_code_id, need_swap, swap_out_task_page_id, swap_in_task_page_id,
-               swap_memory_page_id):
-    print('第' + str(code_num) + '条指令 ' +
-          ' 指令地址' + str(current_code_id).ljust(3) +
-          (' 需要换页 ' if need_swap else ' 不需要换页 ').ljust(12) +
-          ' 换出页' + (str(swap_out_task_page_id) if swap_out_task_page_id != -1 else ' - ').ljust(3) +
-          ' 换入页' + (str(swap_in_task_page_id) if swap_in_task_page_id != -1 else ' - ').ljust(3) +
-          ' 被换页内存页' + (str(swap_memory_page_id) if swap_memory_page_id != -1 else ' - ').ljust(3))
-
-
 class MyManager:
     def __init__(self, page_size, algo):
         self.task_memory_page_amount = 4  # 分配给任务的页面数
@@ -18,12 +8,30 @@ class MyManager:
         self.task_page = [None for i in range(self.task_memory_page_amount)]  # 分配的页面 记录页面号
         self.code_num = 0  # 记录执行到第几条代码
         self.algo = algo  # 记录设定的管理器的算法
+        self.page_allocated_amount = 0
 
         if self.algo == 'FIFO':  # 根据不同的算法配置不同的参数
             self.page_allocate_queue = queue.Queue()  # 记录页面分配顺序
         elif self.algo == 'LRU':
             self.unused_time = [None for i in range(self.task_memory_page_amount)]  # 记录页面未被使用的时间
-            self.page_allocated_amount = 0
+
+    def pageSwap(self, dst_memory_page_id, code_page_id, task):  # 页面调换
+        old_page = self.task_page[dst_memory_page_id]  # 该模拟内存页上存放的旧作业页
+        self.task_page[dst_memory_page_id] = code_page_id  # 写入新作业页
+        task.pcb.page_table[old_page] = -1  # 旧作业页的页表置-1
+        task.pcb.page_table[code_page_id] = dst_memory_page_id  # 新作业页的页表置模拟内存页号
+        return old_page
+
+    def allocateEmptyPage(self, empty_page_id, code_page_id, task):  # 分配空页面
+        self.task_page[empty_page_id] = code_page_id
+        task.pcb.page_table[code_page_id] = empty_page_id
+        self.page_allocated_amount += 1
+
+    def add_unused_time(self, used_page_id):  # 用于LRU算法，增加界面的“未被使用时间”
+        for i in range(self.task_memory_page_amount):
+            if self.unused_time[i] is not None:
+                self.unused_time[i] += 1  # 增加界面的“未被使用时间”
+        self.unused_time[used_page_id] = 0  # 被使用的界面则清空“未被使用时间”
 
     def runTask(self, task):
         if self.algo == 'FIFO':
@@ -34,69 +42,48 @@ class MyManager:
     def runTaskByFIFO(self, task):
         # 从task中获取当前要执行的代码的信息
         current_code_id, memory_page_for_code, code_page_id = task.getCurrentCodeId()
-        # 初始化log
-        log1, log2, log3, log4, log5, log6 = 0, 0, False, -1, -1, -1
+        # 初始化log 用于记录可视化界面所需信息
+        code_num_log, cur_code_log, need_page_log, old_page_log, code_page_log, memory_page_log = 0, 0, False, -1, -1, -1
         if memory_page_for_code != -1:  # 在内存中
-            log3, log4, log6 = False, -1, memory_page_for_code
-        else:  # 不在内存中
-            if self.page_allocate_queue.qsize() < self.task_memory_page_amount:  # 内存没有分配满
-                for i in range(self.task_memory_page_amount):
-                    if self.task_page[i] is None:  # 找到空位置
-                        self.task_page[i] = code_page_id
-                        task.pcb.page_table[code_page_id] = i
-                        self.page_allocate_queue.put(i)
-                        log3, log4, log6 = True, -1, i
-                        break
-            else:  # 内存被分配满 进行页面调换
-                dst_memory_page_id = self.page_allocate_queue.get()  # 从队列中取出最早分配的页序号
-                old_page = self.task_page[dst_memory_page_id]
-                self.task_page[dst_memory_page_id] = code_page_id
-                task.pcb.page_table[old_page] = -1
-                task.pcb.page_table[code_page_id] = dst_memory_page_id
-                self.page_allocate_queue.put(dst_memory_page_id)  # 新分配的页序号插入队尾
-                log3, log4, log6 = True, old_page, dst_memory_page_id
-        log1, log2, log5 = self.code_num, current_code_id, code_page_id
-        # managerLog(log1, log2, log3, log4, log5, log6)
-
-        self.code_num = self.code_num + 1
-        return log1, log2, log3, log4, log5, log6
-
-    def add_unused_time(self):
-        for i in range(self.task_memory_page_amount):
-            if self.unused_time[i] is not None:
-                self.unused_time[i] = self.unused_time[i] + 1
-
-    def runTaskByLRU(self, task):
-        # 从task中获取当前要执行的代码的信息
-        current_code_id, memory_page_for_code, code_page_id = task.getCurrentCodeId()
-        # 初始化log
-        log1, log2, log3, log4, log5, log6 = 0, 0, False, -1, -1, -1
-        if memory_page_for_code != -1:  # 在内存中
-            self.add_unused_time()
-            self.unused_time[memory_page_for_code] = 0
-            log3, log4, log6 = False, -1, memory_page_for_code
+            need_page_log, old_page_log, memory_page_log = False, -1, memory_page_for_code  # 记录可视化界面所需信息
         else:  # 不在内存中
             if self.page_allocated_amount < self.task_memory_page_amount:  # 内存没有分配满
                 for i in range(self.task_memory_page_amount):
                     if self.task_page[i] is None:  # 找到空位置
-                        self.task_page[i] = code_page_id
-                        task.pcb.page_table[code_page_id] = i
-                        self.page_allocated_amount = self.page_allocated_amount + 1
-                        self.add_unused_time()
-                        self.unused_time[i] = 0
-                        log3, log4, log6 = True, -1, i
+                        self.allocateEmptyPage(i, code_page_id, task)  # 分配空的模拟内存页面
+                        self.page_allocate_queue.put(i)  # 插入FIFO算法队列
+                        need_page_log, old_page_log, memory_page_log = True, -1, i  # 记录可视化界面所需信息
+                        break
+            else:  # 内存被分配满 进行页面调换
+                dst_memory_page_id = self.page_allocate_queue.get()  # 从队列中取出最早分配的页序号
+                old_page = self.pageSwap(dst_memory_page_id, code_page_id, task)  # 页面调换
+                self.page_allocate_queue.put(dst_memory_page_id)  # 新分配的页序号插入队尾
+                need_page_log, old_page_log, memory_page_log = True, old_page, dst_memory_page_id  # 记录可视化界面所需信息
+        code_num_log, cur_code_log, code_page_log = self.code_num, current_code_id, code_page_id  # 记录可视化界面所需信息
+        self.code_num += 1
+        return code_num_log, cur_code_log, need_page_log, old_page_log, code_page_log, memory_page_log
+
+    def runTaskByLRU(self, task):
+        # 从task中获取当前要执行的代码的信息
+        current_code_id, memory_page_for_code, code_page_id = task.getCurrentCodeId()
+        # 初始化log 用于记录可视化界面所需信息
+        code_num_log, cur_code_log, need_page_log, old_page_log, code_page_log, memory_page_log = 0, 0, False, -1, -1, -1
+        if memory_page_for_code != -1:  # 在内存中
+            self.add_unused_time(used_page_id=memory_page_for_code)  #
+            need_page_log, old_page_log, memory_page_log = False, -1, memory_page_for_code  # 记录可视化界面所需信息
+        else:  # 不在内存中
+            if self.page_allocated_amount < self.task_memory_page_amount:  # 内存没有分配满
+                for i in range(self.task_memory_page_amount):
+                    if self.task_page[i] is None:  # 找到空位置
+                        self.allocateEmptyPage(i, code_page_id, task)  # 分配空的模拟内存页面
+                        self.add_unused_time(used_page_id=i)  # 更新LRU算法记录的未使用时间
+                        need_page_log, old_page_log, memory_page_log = True, -1, i  # 记录可视化界面所需信息
                         break
             else:  # 内存被分配满 进行页面调换
                 dst_memory_page_id = self.unused_time.index(max(self.unused_time))  # 从队列中取出最久未使用的页序号
-                old_page = self.task_page[dst_memory_page_id]
-                self.task_page[dst_memory_page_id] = code_page_id
-                task.pcb.page_table[old_page] = -1
-                task.pcb.page_table[code_page_id] = dst_memory_page_id
-                self.add_unused_time()
-                self.unused_time[dst_memory_page_id] = 0
-                log3, log4, log6 = True, old_page, dst_memory_page_id
-        log1, log2, log5 = self.code_num, current_code_id, code_page_id
-        # managerLog(log1, log2, log3, log4, log5, log6)
-
-        self.code_num = self.code_num + 1
-        return log1, log2, log3, log4, log5, log6
+                old_page = self.pageSwap(dst_memory_page_id, code_page_id, task)  # 页面调换
+                self.add_unused_time(used_page_id=dst_memory_page_id)  # 更新LRU算法记录的未使用时间
+                need_page_log, old_page_log, memory_page_log = True, old_page, dst_memory_page_id  # 记录可视化界面所需信息
+        code_num_log, cur_code_log, code_page_log = self.code_num, current_code_id, code_page_id  # 记录可视化界面所需信息
+        self.code_num += 1
+        return code_num_log, cur_code_log, need_page_log, old_page_log, code_page_log, memory_page_log
--- code/task.py
+++ code/task.py
@@ -4,8 +4,8 @@ import random
 class Pcb:
     def __init__(self, page_size, code_amount):
         self.task_page_table_size = code_amount // page_size
-        if code_amount % page_size != 0:
-            self.task_page_table_size = self.task_page_table_size + 1
+        # 如果code_amount % page_size不为0，则说明有部分代码无法占满一个页表页面，为这部分代码分配一页页表
+        self.task_page_table_size += (code_amount % page_size != 0)
         self.page_table = [-1 for i in range(self.task_page_table_size)]  # 记录程序的每一页在内存中的哪一页 值为-1则不在内存中
 
 
@@ -21,14 +21,11 @@ class Task:
         tmp_code_id = self.current_code_id
         if self.state == 0:
             self.current_code_id = (self.current_code_id + 1) % self.code_amount  # 顺序执行
-            self.state = 1
         elif self.state == 1:
             self.current_code_id = random.randint(0, max(self.current_code_id - 1, 0))  # 在前面的代码中随机取
-            self.state = 2
         elif self.state == 2:
             self.current_code_id = (self.current_code_id + 1) % self.code_amount  # 顺序执行
-            self.state = 3
         elif self.state == 3:
             self.current_code_id = random.randint(min(self.current_code_id + 1, self.code_amount - 1), self.code_amount - 1)  # 在后面的代码中随机取
-            self.state = 0
+        self.state = (self.state + 1) % 4  # 在四种状态中循环 每一次被调用则切换到下一个状态 并按当前状态的规则生成模拟作业的指令地址
         return tmp_code_id, self.pcb.page_table[tmp_code_id // self.page_size], tmp_code_id // self.page_size
